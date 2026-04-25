d487627b917ac3b32e38830db8d1fb747f13e98f
Govind Shewar <130726235+Doctor-ng@users.noreply.github.com>
2026-04-25 15:13:58 +0530

    Create config_example.py

---
+++ config_example.py
@@ -0,0 +1,10 @@
+# ================= CONFIG =================
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+COINGECKO_API_KEY = "YOUR_API_KEY"
+ADMIN_ID = YOUR_TG_ID
+DB_FILE = "bot.db"
+CACHE_TTL = 60
+CHANNEL_LINKS = [
+    "https://t.me/Doc_Tools", # Your Channel Links
+    "https://t.me/DoctorEX0?start=1" 
+]
